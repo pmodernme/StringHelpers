2f783e3a05a5fd6c1d8fc9d9102abeed18a305cd
Zoe Van Brunt <zoe@zoelog.io>
2025-04-08 14:09:15 -1000

    Remove conflicting overload

--- Sources/StringHelpers/Bundle+Localization.swift
+++ Sources/StringHelpers/Bundle+Localization.swift
@@ -9,15 +9,8 @@
 import Foundation
 
 extension Bundle {
+    @available(*, deprecated, message: "Use localizedString(forKey:) instead")
     public func localizedString(forKey key: String) -> String {
         return localizedString(forKey: key, value: nil, table: nil)
-    }
-}
-
-extension String {
-    public init(localized format: String, bundle: Bundle = Bundle.main, _ arguments: CVarArg...) {
-        let format = bundle.localizedString(forKey: format)
-        let result = String.localizedStringWithFormat(format, arguments)
-        self.init(stringLiteral: result)
     }
 }
